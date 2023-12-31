d40ae8a6119a9ec7c46c73a66aa6cc7bb224b0dc
Alex Bulganin <90492542+doal116@users.noreply.github.com>
2023-12-31 12:12:37 +0200

    feat: completion of functionnality

--- electricity.cpp
+++ electricity.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <cstdlib>
 
 using namespace std;
 
@@ -181,7 +182,7 @@ vector<Billing> cummulativeStorageCalc(vector<Billing> billList)
 vector<Billing> shortageCalc(vector<Billing> billList)
 {
     vector<Billing> resultList = billList;
-    
+
     for (int i = 0; i < resultList.size(); i++)
     {
         float difference = resultList[i].electricConsumption - resultList[i].pvProduction;
@@ -205,7 +206,14 @@ vector<Billing> surPlusCalc(vector<Billing> billList)
 
 vector<Billing> usedCummulativeCalc(vector<Billing> billList)
 {
-    vector<Billing> resultList;
+    vector<Billing> resultList = billList;
+    for (int i = 0; i < resultList.size(); i++)
+    {
+        if (resultList[i].cumulativePv_Kwh >= resultList[i].shortage_Kwh)
+            resultList[i].usedCumlativePv_Kwh = resultList[i].shortage_Kwh;
+        else
+            resultList[i].usedCumlativePv_Kwh = 0;
+    }
 
     return resultList;
 }
@@ -224,11 +232,11 @@ int main()
 
     // shortageCalc kwh
     billList = shortageCalc(billList);
-    displayBillList(billList);
+    // displayBillList(billList);
 
     // surPlusCalc kwh
-    billList = surPlusCalc(billList);
-    displayBillList(billList);
+    // billList = surPlusCalc(billList);
+    // displayBillList(billList);
 
     // usedCummulativeCalc kwh
     billList = usedCummulativeCalc(billList);
